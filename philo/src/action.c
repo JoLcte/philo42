5fee25d5f2cc6e58ae29526421ff03196bdd49c3
Shlurck <shlurck@iShlurck.local>
2022-04-23 13:12:17 +0200

    not working

--- philo/src/action.c
+++ philo/src/action.c
@@ -6,13 +6,13 @@
 /*   By: jlecomte <jlecomte@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/03 17:05:07 by jlecomte          #+#    #+#             */
-/*   Updated: 2022/04/22 16:07:04 by jlecomte         ###   ########.fr       */
+/*   Updated: 2022/04/23 13:11:27 by jlecomte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-static int	dead_sleeping(t_frame *frame, t_philo *philo)
+/*static int	dead_sleeping(t_frame *frame, t_philo *philo)
 {
 	long int	alive;
 
@@ -33,9 +33,9 @@ static int	dead_sleeping(t_frame *frame, t_philo *philo)
 	}
 	pthread_mutex_unlock(frame->lock);
 	return (0);
-}
+}*/
 
-static int	dead_eating(t_frame *frame, t_philo *philo)
+/*static int	dead_eating(t_frame *frame, t_philo *philo)
 {
 	pthread_mutex_lock(frame->lock);
 	if (frame->stop == 1)
@@ -57,9 +57,9 @@ static int	dead_eating(t_frame *frame, t_philo *philo)
 	}
 	pthread_mutex_unlock(frame->lock);
 	return (0);
-}
+}*/
 
-static int	is_dead(t_frame *frame, t_philo *philo, int lock)
+/*static int	is_dead(t_frame *frame, t_philo *philo, int lock)
 {	
 	pthread_mutex_lock(frame->lock);
 	if (frame->stop == 1)
@@ -84,27 +84,27 @@ static int	is_dead(t_frame *frame, t_philo *philo, int lock)
 	}
 	pthread_mutex_unlock(frame->lock);
 	return (0);
-}
+}*/
 
 void	eat_with_forks(t_frame *frame, t_philo *philo)
 {
 	pthread_mutex_lock(philo->l_fork);
-	if (is_dead(frame, philo, 1))
-		return ;
+//	if (is_dead(frame, philo, 1))
+//		return ;
 	pthread_mutex_lock(frame->lock);
 	print_info(frame, philo->id, PHILO_TAKES_FORK, 0);
 	pthread_mutex_lock(philo->r_fork);
-	if (is_dead(frame, philo, 2))
-		return ;
+//	if (is_dead(frame, philo, 2))
+//		return ;
 	pthread_mutex_lock(frame->lock);
 	print_info(frame, philo->id, PHILO_TAKES_FORK, 0);
-	if (is_dead(frame, philo, 2))
-		return ;
+//	if (is_dead(frame, philo, 2))
+//		return ;
 	pthread_mutex_lock(frame->lock);
 	print_info(frame, philo->id, PHILO_EATS, 0);
 	philo->last_ate = _get_time();
-	if (dead_eating(frame, philo))
-		return ;
+//	if (dead_eating(frame, philo))
+//		return ;
 	ft_sleep(frame->setup[EAT]);
 	pthread_mutex_unlock(philo->l_fork);
 	pthread_mutex_unlock(philo->r_fork);
@@ -115,12 +115,12 @@ void	sleep_and_think(t_frame *frame, t_philo *philo)
 	pthread_mutex_lock(frame->lock);
 	if (print_info(frame, philo->id, PHILO_SLEEPS, 0))
 		return ;
-	if (dead_sleeping(frame, philo))
-		return ;
+	//if (dead_sleeping(frame, philo))
+	//	return ;
 	ft_sleep(frame->setup[SLEEP]);
 	pthread_mutex_lock(frame->lock);
 	if (print_info(frame, philo->id, PHILO_THINKS, 0))
 		return ;
-	if (is_dead(frame, philo, 0))
-		return ;
+	//if (is_dead(frame, philo, 0))
+	//	return ;
 }
--- philo/src/thread_handle.c
+++ philo/src/thread_handle.c
@@ -6,7 +6,7 @@
 /*   By: jlecomte <jlecomte@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/02 18:56:34 by jlecomte          #+#    #+#             */
-/*   Updated: 2022/04/20 21:01:58 by jlecomte         ###   ########.fr       */
+/*   Updated: 2022/04/23 13:09:03 by jlecomte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,6 +42,33 @@ static int	stop_all(t_frame *frame)
 	return (0);
 }
 
+static void    is_dead(t_frame *frame)
+{
+	int        i;
+
+	while (1)
+	{
+
+		i = 0;
+		while (i < frame->setup[NB_PHILO])
+		{
+			pthread_mutex_lock(frame->lock);
+			if (_get_time() - frame->philo[i].last_ate >= frame->setup[DIE])
+			{
+				print_info(frame, frame->philo[i].id, PHILO_DIED, 1);
+				frame->stop = 1;
+				pthread_mutex_unlock(frame->lock);
+				break ;
+			}
+			pthread_mutex_unlock(frame->lock);
+			++i;
+		}
+		if (stop_all(frame))
+			return ;
+		usleep(1000);
+	}
+}
+
 static void	*meals_routine(void *data)
 {
 	t_philo	*philo;
@@ -68,25 +95,26 @@ static void	*meals_routine(void *data)
 	return (NULL);
 }
 
-static void	*routine(void *data)
+static void    *routine(void *data)
 {
-	t_philo	*philo;
-	t_frame	*frame;
+    t_philo    *philo;
+    t_frame    *frame;
 
-	philo = (t_philo *)data;
-	frame = philo->frame;
-	if (philo->id % 2 == 0)
-		ft_sleep(frame->setup[EAT] / 2);
-	while (1)
-	{
-		eat_with_forks(frame, philo);
-		if (stop_all(frame))
-			break ;
-		sleep_and_think(frame, philo);
-		if (stop_all(frame))
-			break ;
-	}
-	return (NULL);
+    philo = (t_philo *)data;
+    frame = philo->frame;
+    philo->last_ate = _get_time();
+    if (philo->id % 2 == 0)
+        ft_sleep(frame->setup[EAT] / 2);
+    while (1)
+    {
+        eat_with_forks(frame, philo);
+        if (stop_all(frame))
+            break ;
+        sleep_and_think(frame, philo);
+        if (stop_all(frame))
+            break ;
+    }
+    return (NULL);
 }
 
 void	thread_actions(t_frame *frame)
@@ -96,12 +124,13 @@ void	thread_actions(t_frame *frame)
 
 	i = 0;
 	frame->start = _get_time();
+	is_dead(frame);
 	if (frame->setup[MEALS] > 0)
 	{
 		while (i < nb_philo)
-		{
+			{
 			pthread_create(&frame->philo[i].thd, \
-				NULL, meals_routine, (void *)&frame->philo[i]);
+					NULL, meals_routine, (void *)&frame->philo[i]);
 			++i;
 		}
 	}
@@ -110,7 +139,7 @@ void	thread_actions(t_frame *frame)
 		while (i < nb_philo)
 		{
 			pthread_create(&frame->philo[i].thd, \
-				NULL, routine, (void *)&frame->philo[i]);
+					NULL, routine, (void *)&frame->philo[i]);
 			++i;
 		}
 	}
